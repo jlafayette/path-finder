6239c6b481c8fe36c3abae0f27594b6899063ca9
jlafayette <joshlafayette@gmail.com>
2017-12-16 23:37:15 -0800

    Cleanup old code in game module

--- game.py
+++ game.py
@@ -202,10 +202,7 @@ class DisplayResultState(BaseState):
 def main():
     pygame.init()
     shared = Shared(initialize=True)
-    loop = 0
     while True:
-        loop += 1
-        # print(loop)
         try:
             shared.state.handleinput()
         except RuntimeError:
@@ -214,93 +211,6 @@ def main():
             shared.state.update()
             shared.state.draw()
         shared.clock.tick(shared.state.fps*SPEED_MULT)
-
-    # while not done:
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             done = True
-    #         elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-    #             refresh = True
-    #         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-    #             refresh = True
-    #             step_mode = True
-    #
-    #     # pressed = pygame.key.get_pressed()
-    #     # if pressed[pygame.K_UP]:
-    #     #     y -= 3
-    #     # if pressed[pygame.K_DOWN]:
-    #     #     y += 3
-    #     # if pressed[pygame.K_LEFT]:
-    #     #     x -= 3
-    #     # if pressed[pygame.K_RIGHT]:
-    #     #     x += 3
-    #     screen.fill(color.BLACK)
-    #     if refresh:
-    #         grid.refresh()
-    #         refresh = False
-    #         # for result in grid.draw_step():
-    #         #     pygame.display.update()
-    #         #     clock.tick(480*4)
-    #         grid.draw()
-    #     else:
-    #         grid.draw()
-    #
-    #     pygame.display.update()
-    #     clock.tick(60)
-    #
-    #     if step_mode:
-    #         step_mode = False
-    #
-    #         zoom = False
-    #
-    #         goal_block = None
-    #         steps = 0
-    #         search_generator = breadth_first.search(grid)
-    #         while True:
-    #             if zoom:
-    #                 go_to_next = True
-    #             else:
-    #                 go_to_next = False
-    #             try:
-    #                 for event in pygame.event.get():
-    #                     if event.type == pygame.QUIT:
-    #                         raise UserWarning("DONE!!")
-    #                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-    #                         go_to_next = True
-    #                     elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-    #                         zoom = True
-    #             except UserWarning:
-    #                 break
-    #
-    #             if go_to_next:
-    #                 try:
-    #                     blocks = next(search_generator)
-    #                 except StopIteration:
-    #                     break
-    #                 if blocks[-1].end:
-    #                     goal_block = blocks[-1]
-    #                 steps += 1
-    #
-    #                 # add code here to draw and step
-    #                 for block in blocks:
-    #                     block.draw(screen)
-    #                 pygame.display.update([block.rect for block in blocks])
-    #
-    #                 clock.tick(60*10)
-    #
-    #         print("steps: {}".format(steps))
-    #
-    #         print("goal_block: {!r}".format(goal_block))
-    #         if goal_block is not None:
-    #             for block in breadth_first.construct_path(goal_block):
-    #                 try:
-    #                     block.solve = True
-    #                     block.draw(screen)
-    #                 except (TypeError, AttributeError) as exc:
-    #                     print("caught err: {}".format(exc))
-    #                     pass
-    #                 pygame.display.update()
-    #                 clock.tick(60*4)
 
 
 if __name__ == '__main__':
