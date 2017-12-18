5ef866abf53b1c9a666134f1a57844953c856711
jlafayette <joshlafayette@gmail.com>
2017-12-17 21:05:55 -0800

    Add state display text

--- color.py
+++ color.py
@@ -5,6 +5,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 GREY = (128, 128, 128)
+WHITE = (255, 255, 255)
 
 
 def tint(color, amount=50):
--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 
 import breadth_first
 import a_star
+import color
 from grid import Grid
 
 
@@ -41,6 +42,7 @@ class Shared(Borg):
         Borg.__init__(self)
         if initialize:
             self.screen = pygame.display.set_mode(preset.screen_size)
+            self.preset = preset
             self.clock = pygame.time.Clock()
             self.grid = Grid(self.screen, preset)
             self.generator = None
@@ -52,6 +54,42 @@ SEARCH_DICT = {
     pygame.K_b: breadth_first.search,
     pygame.K_a: a_star.search
 }
+
+
+class StatusDisplay(object):
+
+    def __init__(self, x_pos, y_pos):
+        self.font = pygame.font.SysFont('input', 16)
+        self.x = x_pos
+        self.y = y_pos
+        try:
+            self._text = "Mode: {!s}".format(Shared().state)
+        except AttributeError:
+            self._text = "Mode: INITIALIZING..."
+        self.new_text = True
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        self.new_text = True
+        self._text = value
+
+    def update(self):
+        self.text = "Mode: {!s}".format(Shared().state)
+
+    def draw(self):
+        if self.new_text:
+            self.new_text = False
+            textsurface = self.font.render(self.text, True, color.WHITE, color.BLACK)
+            shared = Shared()
+            rect = pygame.Rect(self.x, self.y,
+                               shared.screen.get_width(), int(shared.preset.buffer/2))
+            pygame.draw.rect(shared.screen, color.BLACK, rect)
+            shared.screen.blit(textsurface, (self.x, self.y))   # returns pygame.Rect
+            pygame.display.update([rect])
 
 
 class BaseState(object):
@@ -77,6 +115,9 @@ class BaseState(object):
     def draw(self):
         pass
 
+    def __str__(self):
+        return "BASE"
+
 
 class CreateState(BaseState):
     def __init__(self):
@@ -90,12 +131,17 @@ class CreateState(BaseState):
         super(CreateState, self).process_event(event)
 
     def update(self):
+        super(CreateState, self).update()
         Shared().grid.refresh()
 
     def draw(self):
+        super(CreateState, self).draw()
         Shared().grid.draw()
         pygame.display.update()
         Shared().state = IdleState()
+
+    def __str__(self):
+        return "CREATE"
 
 
 class IdleState(BaseState):
@@ -112,6 +158,9 @@ class IdleState(BaseState):
             shared.state = SolveState()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             shared.state = StepSolveState()
+
+    def __str__(self):
+        return "IDLE"
 
 
 class BaseSolveState(BaseState):
@@ -134,6 +183,7 @@ class BaseSolveState(BaseState):
             shared.state = self.toggle_state()
 
     def update(self):
+        super(BaseSolveState, self).update()
         shared = Shared()
         try:
             self.updated_blocks = next(shared.generator)
@@ -149,17 +199,24 @@ class BaseSolveState(BaseState):
                 self.goal_block = self.updated_blocks[-1]
 
     def draw(self):
+        super(BaseSolveState, self).draw()
         for block in self.updated_blocks:
             block.draw(Shared().screen)
         pygame.display.update([block.rect for block in self.updated_blocks])
+
+    def __str__(self):
+        return "BASE-SOLVE"
 
 
 class SolveState(BaseSolveState):
     def __init__(self):
         super(SolveState, self).__init__()
-        self.fps = 240
+        self.fps = 120
         self.toggle_state = StepSolveState
         self.toggle_keys = [pygame.K_SPACE, pygame.K_RIGHT]
+
+    def __str__(self):
+        return "SOLVE"
 
 
 class StepSolveState(BaseSolveState):
@@ -182,6 +239,9 @@ class StepSolveState(BaseSolveState):
             # TODO: Add blinking for current block when idle
             pass
 
+    def __str__(self):
+        return "STEP-SOLVE"
+
 
 class SolutionState(BaseState):
     def __init__(self, goal_block):
@@ -189,18 +249,23 @@ class SolutionState(BaseState):
         self.block = goal_block
 
     def update(self):
+        super(SolutionState, self).update()
         if self.block is None:
             Shared().state = DisplayResultState()
         else:
             self.block.solve = True
 
     def draw(self):
+        super(SolutionState, self).draw()
         if self.block is None:
             pass
         else:
             self.block.draw(Shared().screen)
             pygame.display.update([self.block.rect])
             self.block = self.block.parent
+
+    def __str__(self):
+        return "SOLUTION"
 
 
 class DisplayResultState(BaseState):
@@ -214,19 +279,32 @@ class DisplayResultState(BaseState):
         if event.type == pygame.KEYDOWN:   # press any key to continue
             shared.state = CreateState()
 
+    def __str__(self):
+        return "DISPLAY-RESULT"
+
 
 def main():
     pygame.init()
+    pygame.font.init()
     preset = StandardPreset()
     shared = Shared(initialize=True, preset=preset)
+    display = StatusDisplay(0, 0)
+
+    # print("screentype: {}".format(type(shared.screen)))
+
+    loop = 0
     while True:
+        loop += 1
+        # print("loop: {}".format(loop))
         try:
             shared.state.handleinput()
         except RuntimeError:
             break
         else:
             shared.state.update()
+            display.update()
             shared.state.draw()
+            display.draw()
         shared.clock.tick(shared.state.fps * preset.speed_mult)
 
 
